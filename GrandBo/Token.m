7c667e514fc4571a4da965aa23c5e4625b5ded81
MarineXmh <15957299a@gmail.com>
2015-11-02 19:19:05 +0800

    fix bugs

--- GrandBo/Token.m
+++ GrandBo/Token.m
@@ -11,21 +11,21 @@
 @implementation Token
 
 + (NSString *)loadToken {
-    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
+    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
     NSError *error = nil;
     NSString *token = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
     return token;
 }
 
 + (BOOL)saveToken:(NSString *)token {
-    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
+    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
     NSError *error = nil;
     [token writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&error];
     return YES;
 }
 
 + (BOOL)deleteToken {
-    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
+    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingPathComponent:@"token.txt"];
     NSError *error = nil;
     [[NSFileManager defaultManager] removeItemAtPath:path error:&error];
     return YES;
